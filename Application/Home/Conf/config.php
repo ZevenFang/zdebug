30522caef2b6db4937b41367bcffd2ac3ba76086
ZevenFang <fangfan1997@126.com>
2015-05-31 16:37:57 +0800

    修改config文件
    将安装状态设置为未安装

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -2,10 +2,5 @@
 return array (
   'DB_TYPE' => 'mysql',
   'DB_PREFIX' => 'zd_',
-  'installed' => true,
-  'DB_HOST' => 'localhost',
-  'DB_NAME' => 'zdebug',
-  'DB_USER' => 'root',
-  'DB_PWD' => 'ff8613',
-  'DB_PORT' => '3306',
+  'installed' => false,
 );
